c39bc8418a6a48973fa75b78cea26890b8fbb92a
james0831 <jdcarroll08@gmail.com>
2017-07-24 15:49:02 -0700

    Adds Question class

--- surveys/survey_view.php
+++ surveys/survey_view.php
@@ -34,19 +34,22 @@ $mySurvey = new Survey($myID);
 
 if($mySurvey->IsValid)
 {#only load data if record found
-	$config->titleTag = $mySurvey->Title . " surveys made with php and love!"; #overwrite PageTitle with Muffin info!
+	$config->titleTag = $mySurvey->Title . " surveys made with php and love!"; #overwrite PageTitle with info!
 	#Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
 }
 
 # END CONFIG AREA ---------------------------------------------------------- 
 
 get_header(); #defaults to theme header or header_inc.php
+
+
 if($mySurvey->IsValid)
 {#records exist - show survey!
 	echo '
 	<h3 align="center">' . $mySurvey->Title . '</h3>
 	<p>' . $mySurvey->Description . '</p>
 	';
+	echo $mySurvey->showQuestions();
 }else{//no such survey!
     echo '<div align="center">What! No such survey? There must be a mistake!!</div>';
     
@@ -62,14 +65,14 @@ class Survey
 	public $Title = '';
   	public $Description = '';
 	public $IsValid = false;
+	public $Questions = array();
     
     public function __construct($id)
     {
         $id = (int)$id; //cast to integer disallows SQL injection
         $sql = "select Title,Description from sm17_surveys where SurveyID = " . $id;
         
-    
-  
+   
 		# connection comes first in mysqli (improved) function
 		$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
@@ -84,6 +87,64 @@ class Survey
 		}
 
 		@mysqli_free_result($result); # We're done with the data!
-    
+
+		
+		
+		//----start question class data here
+		$sql = "select Question,QuestionID,Description from sm17_questions where SurveyID = " . $id;
+  
+		# connection comes first in mysqli (improved) function
+		$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+		if(mysqli_num_rows($result) > 0)
+		{#records exist - process
+			$this->IsValid = true;//record found!
+			while ($row = mysqli_fetch_assoc($result))
+			{
+				//$this->Title = dbOut($row['Title']);
+				//$this->Description = dbOut($row['Description']);
+				$this->Questions[] = new Question(dbOut($row['QuestionID']),dbOut($row['Question']),dbOut($row['Description']));
+			}
+		}
+
+		@mysqli_free_result($result); # We're done with the data!
+		//-----end question class data here
+			
+		
 	}//end Survey __construct
+	
+	public function showQuestions()
+	{
+		$myReturn = '';
+		
+		foreach($this->Questions as $question)
+		{
+			echo'
+			<p>QuestionID: ' . $question->QuestionID . ' Text: ' . $question->Text . ' Description: ' . $question->Description . '</p>
+			
+			
+			';
+		}
+		
+		return $myReturn;
+		
+	}//end of showQuestions
 }//end Survey class
+
+class Question
+{
+	public $QuestionID = 0;
+	public $Text = '';
+	public $Description = '';
+	
+	public function __construct($QuestionID, $Text, $Description)
+	{
+		$this->QuestionID = $QuestionID;
+		$this->Text = $Text;
+		$this->Description = $Description;
+		
+		
+		
+	}//end of constructor
+	
+}//end Question class
